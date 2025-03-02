fb9f82377d0156606fb51c8c15b7ea6371be1819
filippo16 <filippo.lutterotti2@gmail.com>
2025-03-02 11:45:09 +0100

    version

--- src/app.ts
+++ src/app.ts
@@ -16,25 +16,13 @@ const app = express();
 const PORT = process.env.PORT || 443;
 
 // Middleware
-app.use(cors());
 app.use(bodyParser.json());
 
 // Routes
 app.use(sseRoutes);
 app.use(rot);
 
-// app.use(express.static("dist-frontend"));
 
-// const sslOptions = {
-//     key: fs.readFileSync(path.join(__dirname, 'miocert.key')),
-//     cert: fs.readFileSync(path.join(__dirname, 'miocert.crt'))
-// };
-//   // /etc/ssl/private/miocert.key
-// ///etc/ssl/certs/miocert.crt
-// // Avvio del server HTTPS
-// https.createServer(sslOptions, app).listen(PORT, () => {
-// console.log(`Server in ascolto su https://localhost:${PORT}`);
-// });
 
 app.listen(PORT, () => {
     console.log(`Server in ascolto su http://localhost:${PORT}`);
